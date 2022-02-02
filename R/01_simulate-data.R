e49d4f84ab5e4600ddb1fa86358308213b0e1809
Mark Scheuerell <scheuerl@uw.edu>
2022-02-02 14:55:40 -0800

    created options for random and aggregated

--- R/01_simulate-data.R
+++ R/01_simulate-data.R
@@ -13,27 +13,56 @@ y_max <- 100
 ## number of fish/clusters
 n_fish <- 100
 
+## for calculating nearest neighbor distances
+library(spatstat)
+
 
 #### case 1: random ####
 
 ## assumes nearest neighbor distances ~ Poisson(lambda)
 
 ## locations of fish/clusters
-rx_loc <- runif(n_fish, 0, x_max)
-ry_loc <- runif(n_fish, 0, y_max)
+rdm_x_loc <- runif(n_fish, 0, x_max)
+rdm_y_loc <- runif(n_fish, 0, y_max)
+
+## quick plot of locations
+plot(rdm_x_loc, rdm_y_loc, pch = 16, xlim = c(0, 100), ylim = c(0, 100))
+
+## calculate nearest neighbor distances
+rdm_nnd <- nndist(rdm_x_loc, rdm_y_loc)
+hist(rdm_nnd)
 
 
-plot(rx_loc, ry_loc, pch = 16, xlim = c(0, 100), ylim = c(0, 100))
-
-
-#### case 2: aggregated ####
+#### case 2a: aggregated ####
 
 ## assumes nearest neighbor distances ~ NegBin(size, mu)
 
-## locations of fish/clusters
-ax_loc <- rlnorm(n_fish, 3, 0.7)
-ay_loc <- rlnorm(n_fish, 3, 0.7)
+## locations of fish/clusters as log-normal
+agg_x_loc_a <- rlnorm(n_fish, 3, 0.7)
+agg_y_loc_a <- rlnorm(n_fish, 3, 0.7)
+
+## quick plot of locations
+plot(agg_x_loc_a, agg_y_loc_a, pch = 16, xlim = c(0, 100), ylim = c(0, 100))
+
+## calculate nearest neighbor distances
+agg_nnd_a <- nndist(agg_x_loc_a, agg_y_loc_a)
+hist(nnd_a)
 
 
-plot(ax_loc, ay_loc, pch = 16, xlim = c(0, 100), ylim = c(0, 100))
+#### case 2b: aggregated ####
+
+## assumes nearest neighbor distances ~ NegBin(size, mu)
+
+## locations of fish/clusters as normal
+agg_x_loc_b <- rnorm(n_fish, 50, 20)
+agg_y_loc_b <- rnorm(n_fish, 50, 20)
+
+## quick plot of locations
+plot(agg_x_loc_b, agg_y_loc_b, pch = 16, xlim = c(0, 100), ylim = c(0, 100))
+
+## calculate nearest neighbor distances
+agg_nnd_b <- nndist(agg_x_loc_b, agg_y_loc_b)
+hist(agg_nnd_b)
+
+
 
